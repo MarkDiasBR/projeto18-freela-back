1004dccc01c68ce3ab61e42679df7caf2edd17a6
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-05-26 00:16:43 -0300

    build: create app.js

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express, { json } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import chalk from 'chalk';
+dotenv.config();
+
+const server = express();
+server.use(json());
+server.use(cors());
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log(chalk.bgMagenta('\n [Node.js/Express] Server connected SUCCESSFULLY! '), '🖥️⚙️✨');
+    console.log(chalk.white(' Server connected on PORT: '), chalk.blue.italic.underline(`${port}`));
+});
